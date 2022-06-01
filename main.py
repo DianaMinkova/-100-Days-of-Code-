d486dfe25845c777ec18f9b473e9edbf8cbfe1eb
Диана Минкова <dianaminkova@yahoo.com>
2022-06-01 17:59:27 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from coffee_maker import CoffeeMaker
+from menu import Manu
+from money_mashine import MoneyMachine
+
+coffee_maker = CoffeeMaker()
+menu = Manu()
+money_machine = MoneyMachine()
+
+is_on = True
+
+while is_on:
+    option = menu.get_item()
+    choice = input(f'What would you like: ({option}): ')
+    if choice == 'off':
+        is_on = False
+    elif choice == 'report':
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
