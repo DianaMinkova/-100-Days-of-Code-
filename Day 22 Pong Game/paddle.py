a1adc4474f831025ae340a3343c88d5483af5ab0
Диана Минкова <dianaminkova@yahoo.com>
2022-06-24 23:19:11 +0300

    Update paddle.py

--- Day 22 Pong Game/paddle.py
+++ Day 22 Pong Game/paddle.py
@@ -1,20 +1,19 @@
-from turtle import Turtle
+from turtle import Turtle                                  # Creates and returns a new turtle object.
 
 
-class Paddle(Turtle):
+class Paddle(Turtle):                                      # Create class Paddle with inheritance Turtle.
 
-    def __init__(self, position):
-        super().__init__()
-
+    def __init__(self, position):                          # The self variable and other argument
+        super().__init__()                                 # represents the instance of the object
         self.shape('square')
         self.color('white')
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
         self.goto(position)
 
-    def go_up(self):
-        new_y = self.ycor() + 20
-        self.goto(self.xcor(), new_y)
+    def go_up(self):                                       # A method of moving a turtle
+        new_y = self.ycor() + 20                           # Current position of the turtle => x/ycor()
+        self.goto(self.xcor(), new_y)                      # Moves the turtle from the current pos to the location x, y
 
     def go_down(self):
         new_y = self.ycor() - 20
