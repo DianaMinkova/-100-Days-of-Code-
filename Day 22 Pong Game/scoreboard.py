f184288ac750eceffd217f96b99b8ba615a0390a
Диана Минкова <dianaminkova@yahoo.com>
2022-06-24 23:20:20 +0300

    Update scoreboard.py

--- Day 22 Pong Game/scoreboard.py
+++ Day 22 Pong Game/scoreboard.py
@@ -2,25 +2,24 @@ from turtle import Turtle
 
 
 class Scoreboard(Turtle):
-
     def __init__(self):
         super().__init__()
         self.color('white')
         self.penup()
         self.hideturtle()
-        self.l_score = 0
+        self.l_score = 0             # Counting score
         self.r_score = 0
-        self.update_scoreboard()
+        self.update_scoreboard()     # Updating result
 
     def update_scoreboard(self):
-        self.clear()
-        self.goto(-100, 200)
-        self.write(self.l_score, align='center', font=('Courier', 80, 'normal'))
+        self.clear()                 # Clear result on display
+        self.goto(-100, 200)         # Position and font a left score
+        self.write(self.l_score, align='center', font=('Courier', 70, 'normal'))
         self.goto(100, 200)
-        self.write(self.r_score, align='center', font=('Courier', 80, 'normal'))
+        self.write(self.r_score, align='center', font=('Courier', 70, 'normal'))
 
     def l_point(self):
-        self.l_score += 1
+        self.l_score += 1           # Increase score when paddle missed
         self.update_scoreboard()
 
     def r_point(self):
