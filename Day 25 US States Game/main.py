7e2ed829b3bc0b41ee0ba5ab650f5e7a056d662a
Диана Минкова <dianaminkova@yahoo.com>
2022-07-08 01:55:15 +0300

    Update main.py

--- Day 25 US States Game/main.py
+++ Day 25 US States Game/main.py
@@ -1,32 +1,39 @@
-import turtle
-import pandas
+from turtle import Screen, Turtle, shape, mainloop
+from pandas import DataFrame, read_csv
+screen = Screen()
+screen.title('U.S. States Game') # Title name of the game
 
-screen = turtle.Screen()
-screen.title("U.S. States Game")
-image = "blank_states_img.gif"
-screen.addshape(image)
-turtle.shape(image)
+t = Turtle()
+t.penup()
+t.hideturtle()
 
-data = pandas.read_csv("50_states.csv")
-all_states = data.state.to_list()
-guessed_states = []
+image = 'blank_states_img.gif' # Only image.gif file format
+screen.addshape(image) # This function is used to Add a shape to TurtleScreen.
 
-while len(guessed_states) < 50:
-    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
-                                    prompt="What's another state's name?").title()
-    if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
-        new_data = pandas.DataFrame(missing_states)
-        new_data.to_csv("states_to_learn.csv")
+shape(image)   # Set the shape
+
+data = read_csv('50_states.csv')
+# print(data)
+state_list = data.state.to_list()  # Get all state in a list
+# print(state_list)
+guessed_state = [] # Add the correct answer in a new list
+
+while len(guessed_state) <= 50:  # It is value in state
+    answer_state = screen.textinput(title=f'{len(guessed_state)}/50 State Counter', prompt='What\'s another state is name?').title()
+    if answer_state == 'Exit':
+        # Check which states are unknown
+        li_dif = [i for i in state_list + guessed_state if i not in state_list or i not in guessed_state]
+        df = DataFrame(li_dif)
+        print(df)
+        df.to_csv('Learn_more.csv')
         break
-    if answer_state in all_states:
-        guessed_states.append(answer_state)
-        t = turtle.Turtle()
-        t.hideturtle()
-        t.penup()
-        state_data = data[data.state == answer_state]
-        t.goto(int(state_data.x), int(state_data.y))
+
+    if answer_state in state_list:
+        guessed_state.append(answer_state)
+        coordinate = data[data.state == answer_state]
+        # x = coordinate['x'].values[0]
+        # y = coordinate['y'].values[0]
+        # print(x, y)
+        # t.goto(x, y)
+        t.goto(int(coordinate.x), int(coordinate.y))
         t.write(answer_state)
